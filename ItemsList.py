0ddb107537ff3cd39aeac82eac1967dc4fcf2f88
2rii <muhammadraza12108@gmail.com>
2025-07-24 02:54:08 +0400

    Add files via upload

---
+++ ItemsList.py
@@ -0,0 +1,154 @@
+#Items list for use in the main program (kinda like an index file)
+
+weapons = {
+    "blade of honor": {
+        "buy_price": 15,
+        "damage": 15,
+        "durability": 30,
+        "code": "BH",
+        "sell_price": 7
+    },
+    "blade of glory": {
+        "buy_price": 15,
+        "damage": 30,
+        "durability": 15,
+        "code": "BG",
+        "sell_price": 5
+    },
+    "blade of the east": {
+        "buy_price": 30,
+        "damage": 30,
+        "durability": 30,
+        "code": "BE",
+        "sell_price": 12
+    },
+    "blade of severance": {
+        "buy_price": 60,
+        "damage": 40,
+        "durability": 100,
+        "code": "BS",
+        "sell_price": 20
+    },
+    "ayutthayan crossbow": {
+        "buy_price": 5,
+        "damage": 5,
+        "durability": 20,
+        "code": "AC",
+        "sell_price": 2
+    },
+    "riz's bow": {
+        "buy_price": 10,
+        "damage": 10,
+        "durability": 20,
+        "code": "RB",
+        "sell_price": 5
+    },
+    "axe of the millions": {
+        "buy_price": 15,
+        "damage": 20,
+        "durability": 5,
+        "code": "AM",
+        "sell_price": 7
+    },
+    "the reaper's dagger": {
+        "buy_price": 2,
+        "damage": 5,
+        "durability": 10,
+        "code": "RD",
+        "sell_price": None 
+    },
+    "caroline's mace": {
+        "buy_price": 20,
+        "damage": 30,
+        "durability": 2,
+        "code": "CM",
+        "sell_price": 5
+    },
+    "hunting spear": {
+        "buy_price": 1,
+        "damage": 5,
+        "durability": 10,
+        "code": "HS",
+        "sell_price": None
+    }
+}
+
+potions = {
+    "potion of healing": {
+        "buy_price": 3,
+        "stamina_increase": 10,
+        "stamina_increasemax": 0,
+        "health_increase": 30,
+        "health_increasemax": 0,
+        "sell_price": None
+    },
+    "potion of epiphany": {
+        "buy_price": 6,
+        "stamina_increase": 15,
+        "stamina_increasemax": 0,
+        "health_increase": 50,
+        "health_increasemax": 0,
+        "sell_price": None
+    },
+    "bastardized potion of healing": {
+        "buy_price": 15,
+        "stamina_increase": 30,
+        "stamina_increasemax": 0,
+        "health_increase": 50,
+        "health_increasemax": 5,
+        "sell_price": None
+    },
+    "bastardized potion of epiphany": {
+        "buy_price": 40,
+        "stamina_increase": 40,
+        "stamina_increasemax": 10,
+        "health_increase": 100,
+        "health_increasemax": 10,
+        "sell_price": None
+    },
+    "potion of tenacity": {
+        "buy_price": 15,
+        "stamina_increase": 40,
+        "stamina_increasemax": 0,
+        "health_increase": 0,
+        "health_increasemax": 0,
+        "sell_price": None
+    },
+    "cursed potion of tenacity": {
+        "buy_price": 35,
+        "stamina_increase": 100,
+        "stamina_increasemax": 20,
+        "health_increase": 30,
+        "health_increasemax": 0,
+        "sell_price": None
+    }
+}
+
+drinks = {
+    "water": {
+        "buy_price": 1,
+        "stamina_increase": 5,
+        "health_decrease": 0,
+        "sell_price": None
+    },
+    "beer": {
+        "buy_price": 2,
+        "stamina_increase": 7.5,
+        "health_decrease": 0.5,
+        "sell_price": None
+    },
+    "whiskey": {
+        "buy_price": 5,
+        "stamina_increase": 10,
+        "health_decrease": 1,
+        "sell_price": None
+    },
+    "vodka": {
+        "buy_price": 7.5,
+        "stamina_increase": 25,
+        "health_decrease": 5,
+        "sell_price": None
+    }
+}
+
+
